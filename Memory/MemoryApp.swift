b8b8acc84165e0bf84b9c585652a99d8318149a9
slama-pakhrin <salonlama47@gmail.com>
2024-01-10 13:37:03 -0500

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Arii on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
